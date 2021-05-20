579f1b77af79a5b1fc78a3ca4f305a9b804e781b
Peter Klein <kleinpa00@gmail.com>
2021-05-19 21:04:08 -0400

    Update demonstration routed numpad PCB

--- kbtb/testdata/dist.sh
+++ kbtb/testdata/dist.sh
@@ -5,53 +5,41 @@ files=()
 
 targets+=("//kbtb/testdata:test_layout_svg")
 files+=("kbtb/testdata/test_layout.svg")
-
 targets+=("//kbtb/testdata:test_plate_kicad")
 files+=("kbtb/testdata/plate.kicad_pcb")
-
 targets+=("//kbtb/testdata:test_plate_pcb")
 files+=("kbtb/testdata/test_plate_pcb.zip")
-
 targets+=("//kbtb/testdata:test_board_kicad")
 files+=("kbtb/testdata/board.kicad_pcb")
-
-targets+=("//kbtb/testdata:test_qmk")
-files+=("kbtb/testdata/test_qmk.h")
-
 targets+=("//kbtb/testdata:test_top_plate")
 files+=("kbtb/testdata/test_top_plate.dxf")
 targets+=("//kbtb/testdata:test_bottom_plate")
 files+=("kbtb/testdata/test_bottom_plate.dxf")
+targets+=("//kbtb/testdata:test_qmk")
+files+=("kbtb/testdata/test_qmk.h")
+
 
 targets+=("//kbtb/testdata:tkl_layout_svg")
 files+=("kbtb/testdata/tkl_layout.svg")
 
+
+targets+=("//kbtb/testdata:numpad_kicad")
+files+=("kbtb/testdata/numpad_board.kicad_pcb")
+targets+=("//kbtb/testdata:numpad_plate_pcb")
+files+=("kbtb/testdata/numpad_plate_pcb.zip")
+targets+=("//kbtb/testdata:numpad_layout_svg")
+files+=("kbtb/testdata/numpad_layout.svg")
 targets+=("//kbtb/testdata:numpad_top_plate")
 files+=("kbtb/testdata/numpad_top_plate.dxf")
 targets+=("//kbtb/testdata:numpad_bottom_plate")
 files+=("kbtb/testdata/numpad_bottom_plate.dxf")
+targets+=("//kbtb/testdata/routed:numpad")
+files+=("kbtb/testdata/routed/numpad.zip")
 
-targets+=("//kbtb/testdata:numpad_kicad")
-files+=("kbtb/testdata/numpad_board.kicad_pcb")
+targets+=("//kbtb/testdata:stm32f072_demo_kicad")
+files+=("kbtb/testdata/stm32f072_demo.kicad_pcb")
 
-targets+=("//kbtb/testdata:numpad_plate_pcb")
-files+=("kbtb/testdata/numpad_plate_pcb.zip")
 
-targets+=("//kbtb/testdata:numpad_layout_svg")
-files+=("kbtb/testdata/numpad_layout.svg")
-
-targets+=("//kbtb/testdata:min_stm32_kicad")
-files+=("kbtb/testdata/min_stm32.kicad_pcb")
-
-targets+=("//kbtb/testdata/routed:numpad_board_routed")
-files+=("kbtb/testdata/routed/numpad_board_routed.zip")
-
-targets+=("//kbtb/testdata:atmega32u4_demo_kicad")
-files+=("kbtb/testdata/atmega32u4_demo.kicad_pcb")
-targets+=("//kbtb/testdata/routed:atmega32u4_demo")
-files+=("kbtb/testdata/routed/atmega32u4_demo.zip")
-targets+=("//kbtb/testdata/routed:atmega32u4_demo_bom")
-files+=("kbtb/testdata/routed/atmega32u4_demo_bom.zip")
 
 bazelisk build ${targets[@]} $@
 rm -rf $(bazelisk info workspace)/dist/*
